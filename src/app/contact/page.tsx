ccd6f9876501c4376fe96a651ca2b8f35b87a6eb
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-04-29 07:19:24 +0700

    Add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./contact.scss";
+import { HiMiniPaperAirplane } from "react-icons/hi2";
+type Props = {};
+
+export default function ContactPage({}: Props) {
+  return (
+    <main id="contact-page">
+      <form action="#" className="contact-form">
+        <div className="form-header">
+          <h2>contact us</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
+            quibusdam sint, dolore recusandae sit quam eligendi reiciendis
+            sapiente error excepturi nisi culpa corporis voluptate possimus.
+          </p>
+        </div>
+
+        <div className="form-fields">
+          <div className="fields">
+            <label htmlFor="name">- NAME -</label>
+            <input
+              type="text"
+              autoComplete="name"
+              placeholder="Enter your name here . . ."
+              id="name"
+            />
+          </div>
+          <div className="fields">
+            <label htmlFor="email">- EMAIL -</label>
+            <input
+              type="email"
+              placeholder="Enter your email here . . ."
+              autoComplete="name"
+              id="email"
+            />
+          </div>
+          <div className="fields">
+            <label htmlFor="message">- MESSAGE -</label>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Write your messages here . . ."
+            ></textarea>
+          </div>
+        </div>
+
+        <div className="action">
+          <button type="submit" className="btn btn-submit">
+            <div className="decor"></div>
+            <span className="panel">
+              SEND <HiMiniPaperAirplane />
+            </span>
+          </button>
+        </div>
+
+        <img src="/decors/contact-l.png" className="decors decor-l"></img>
+        <img src="/decors/contact-r.png" className="decors decor-r"></img>
+      </form>
+    </main>
+  );
+}
